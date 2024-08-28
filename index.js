e561a889ebc9a57d42ae2c281d9b0e20eaf2c97c
Alex Surprenant <102190687+Soup098@users.noreply.github.com>
2024-08-27 22:08:03 -0600

    update

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const itemsBox = document.getElementById("items-box")
+const postButton = document.getElementById("post-button")
+const postTitle = document.getElementById("post-title")
+const postBody = document.getElementById("post-body")
+
 
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(results => results.json())
@@ -14,5 +18,19 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
         console.log(postArr)
     })
 
-
-    
+postButton.addEventListener("click",function(event){
+    event.preventDefault()
+    const newPost = {
+        newPostTitle: postTitle.value,
+        newPostBody: postBody.value
+    }
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newPost),
+    })
+        .then(response => response.json())
+        .then(data => console.log(data))
+})
