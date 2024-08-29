206d8c8472ebab7596015a98c878bef13c9ef15b
Alex Surprenant <102190687+Soup098@users.noreply.github.com>
2024-08-28 22:36:37 -0600

    update

--- index.js
+++ index.js
@@ -3,34 +3,49 @@ const postButton = document.getElementById("post-button")
 const postTitle = document.getElementById("post-title")
 const postBody = document.getElementById("post-body")
 
+let postArr = []
+
+function renderPosts(){
+    itemsBox.innerHTML = ""
+    for(let post of postArr){
+        itemsBox.innerHTML += `
+            <h1>${post.title}</h1>
+            <p>${post.body}</p>
+            <hr />
+        `
+    }
+}
 
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(results => results.json())
     .then(data =>{
-        const postArr = data.slice(0, 5)
-        for(let post of postArr){
-            itemsBox.innerHTML += `
-                <h1>${post.title}</h1>
-                <p>${post.body}</p>
-                <hr />
-            `
-        }
+        postArr = data.slice(0, 5)
+        renderPosts()
         console.log(postArr)
     })
 
 postButton.addEventListener("click",function(event){
     event.preventDefault()
     const newPost = {
-        newPostTitle: postTitle.value,
-        newPostBody: postBody.value
+        title: postTitle.value,
+        body: postBody.value
     }
-    fetch("https://apis.scrimba.com/jsonplaceholder/posts", {
+    postTitle.value = ""
+    postBody.value = ""
+    
+    
+    const options = {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(newPost),
-    })
+    }
+
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(post =>{
+            postArr.unshift(post)
+            renderPosts()
+        })
 })
